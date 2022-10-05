1a6bc53633446ebdc1dcef84adbfacf695245975
William Killett <williamkillettiii@yahoo.com>
2022-10-04 18:48:44 -0700

    commit tree e9c65ba97cef9f0bfaec17643f40ebd52eefdf11

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 const init = () => {
-  
+  form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    input = e.target['searchByID'].value
+h4.innerText = fetch(`http://localhost:3000/movies/${input}.title`)
+
+package.innerText = fetch(`http://localhost:3000/movies/${input}.summary`)
+  })
+
 }
 
 document.addEventListener('DOMContentLoaded', init);
